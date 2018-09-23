67d4d521caa64c6886450cf5ab126d9d9f714b2a
9Z29FC2-PC\rkhaled <rkhaled@tistatech.com>
2018-09-23 14:29:34 -0400

    first commit

---
+++ src/main/java/guru/springframework/recipeproject/domain/ingredient.java
@@ -0,0 +1,11 @@
+package guru.springframework.recipeproject.domain;
+
+import java.math.BigDecimal;
+
+public class ingredient {
+
+    private String description;
+    private BigDecimal amount;
+
+    //private UnitOfMeasure uom;
+}
